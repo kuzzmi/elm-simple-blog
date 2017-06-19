66457ec965256213d21e494bd33e77f141d178a5
Igor Kuzmenko <igor@kuzzmi.com>
2017-06-19 16:49:12 +0200

    Installing packages on script stage

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,12 +3,10 @@
 #     $TRAVIS_BUILD_DIR/client/dist/ \
 #     kuzzmi@dev.kuzzmi.com:/var/www/html/dev.kuzzmi.com/
 
-cd $TRAVIS_BUILD_DIR/api/ && npm install && \
-    rsync -rR --quiet --chmod=F550 \
-        $TRAVIS_BUILD_DIR/api/ \
-        kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/api/
+rsync -rR --quiet --chmod=F550 \
+    $TRAVIS_BUILD_DIR/api/ \
+    kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/api/
 
-cd $TRAVIS_BUILD_DIR/cache/ && npm install && \
-    rsync -rR --quiet --chmod=F550 \
-        $TRAVIS_BUILD_DIR/cache/ \
-        kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/cache/
+rsync -rR --quiet --chmod=F550 \
+    $TRAVIS_BUILD_DIR/cache/ \
+    kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/cache/
