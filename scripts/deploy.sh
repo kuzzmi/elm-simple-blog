a0614d2a7dbff0d7d9c01c51be84db8c8811178b
Igor Kuzmenko <igor@kuzzmi.com>
2017-06-20 01:24:54 +0200

    Updates scripts

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,16 @@
 # /usr/bin/rsync -r --delete-after --quiet --chmod=F550 \
 #     $1/client/dist/ \
 #     kuzzmi@dev.kuzzmi.com:/var/www/html/dev.kuzzmi.com/
+TRAVIS_BUILD_DIR=$1
 
-rsync -r \
-    $1/api/ \
+rsync -r --quiet --chmod=F550 \
+    $TRAVIS_BUILD_DIR/api/ \
     kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/api/
 
-rsync -r \
-    $1/cache/ \
+rsync -r --quiet --chmod=F550 \
+    $TRAVIS_BUILD_DIR/cache/ \
     kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/cache/
+
+rsync --quiet --chmod=F550 \
+    $TRAVIS_BUILD_DIR/pm2-application.json \
+    kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/
