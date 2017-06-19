8091e70f58c3df6c1312ecf60a706d9664d20bb1
Igor Kuzmenko <igor@kuzzmi.com>
2017-06-20 00:16:41 +0200

    Testing new deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,9 @@
 #     kuzzmi@dev.kuzzmi.com:/var/www/html/dev.kuzzmi.com/
 
 rsync -rR --quiet --chmod=F550 \
-    $TRAVIS_BUILD_DIR/api/ \
+    ../api/ \
     kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/api/
 
 rsync -rR --quiet --chmod=F550 \
-    $TRAVIS_BUILD_DIR/cache/ \
+    ../cache/ \
     kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/cache/
