5c050b8c918e9d81dd5c77bd8679c3bacd934465
Igor Kuzmenko <igor@kuzzmi.com>
2017-06-20 00:43:04 +0200

    Testing another deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-# rsync -r --delete-after --quiet --chmod=F550 \
-#     $TRAVIS_BUILD_DIR/client/dist/ \
+# /usr/bin/rsync -r --delete-after --quiet --chmod=F550 \
+#     $1/client/dist/ \
 #     kuzzmi@dev.kuzzmi.com:/var/www/html/dev.kuzzmi.com/
 
-rsync -rR --quiet --chmod=F550 \
-    $TRAVIS_BUILD_DIR/api/ \
+/usr/bin/rsync -r --quiet --chmod=F550 \
+    $1/api/ \
     kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/api/
 
-rsync -rR --quiet --chmod=F550 \
-    $TRAVIS_BUILD_DIR/cache/ \
+/usr/bin/rsync -r --quiet --chmod=F550 \
+    $1/cache/ \
     kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/cache/
