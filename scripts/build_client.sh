114a26f177497716df90884fb9467b8d4a740562
Igor Kuzmenko <igor@kuzzmi.com>
2017-06-20 15:34:55 +0200

    Testing another deployment script

--- scripts/build_client.sh
+++ scripts/build_client.sh
@@ -1,1 +1,1 @@
-./should_build.sh $1/$2 && cd $1/$2 && yarn install && $1/sysconfcpus/bin/sysconfcpus -n 2;yarn build
+./should_build.sh $1/$2 && npm i -g elm && cd $1/$2 && yarn install && $1/sysconfcpus/bin/sysconfcpus -n 2;yarn build
