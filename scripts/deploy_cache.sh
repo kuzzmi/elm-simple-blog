d304e1a7433d85bb842b149385d3c6d2ed381e8b
Igor Kuzmenko <igor@kuzzmi.com>
2017-06-20 16:30:29 +0200

    Building will never fail

--- scripts/deploy_cache.sh
+++ scripts/deploy_cache.sh
@@ -1,1 +1,3 @@
 rsync -r --quiet $1/$2 $1/pm2-application.json kuzzmi@dev.kuzzmi.com:/var/www/app/dev.kuzzmi.com/
+ssh kuzzmi@dev.kuzzmi.com -i /tmp/deploy_rsa "/bin/bash -c \"source ~/.nvm/nvm.sh && pm2 restart all\""
+
