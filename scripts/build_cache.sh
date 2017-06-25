44776994479783265f182cf98fb4d12b075ff9e8
Igor Kuzmenko <igor@kuzzmi.com>
2017-06-25 18:28:39 +0200

    Updated build scripts to always build

--- scripts/build_cache.sh
+++ scripts/build_cache.sh
@@ -1,1 +1,1 @@
-$1/scripts/should_build.sh $1/$2 && echo "Nothing required for $2 building"
+echo "Nothing required for $2 building"
--- scripts/build_client.sh
+++ scripts/build_client.sh
@@ -1,1 +1,1 @@
-$1/scripts/should_build.sh $1/$2 && npm i -g elm && cd $1/$2 && $1/sysconfcpus/bin/sysconfcpus -n 2 yarn build
+npm i -g elm && cd $1/$2 && $1/sysconfcpus/bin/sysconfcpus -n 2; yarn build
